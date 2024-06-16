810fcad7ed6caba54b0a14dbc46894a55cd89e41
ozgekaraca <ozgekaraca17@gmail.com>
2024-06-16 20:37:37 +0300

    Hero component created

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,18 @@
 import React from "react";
+import "./Hero.css";
 
 const Hero = () => {
   return (
     <section className="hero">
-      <h2>Hero Component</h2>
-      <p>This is the hero section of the page.</p>
+      <div className="hero-content">
+        <h3>Discover the gym closest to you!</h3>
+        <div className="search-bar">
+          <input type="text" placeholder="Enter city or town" />
+          <button type="button">Search</button>
+        </div>
+      </div>
     </section>
   );
 };
 
-export default Hero; // Hero bileşenini export ediyoruz
+export default Hero;
